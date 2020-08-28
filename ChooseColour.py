603ab5f8d1f311cba3d42d3095e17503381e71c9
Getpriyanka-20 <70354405+Getpriyanka-20@users.noreply.github.com>
2020-08-27 18:11:44 -0700

    Add files via upload

---
+++ ChooseColour.py
@@ -0,0 +1,11 @@
+a=int(input("Choose the number between 1 to 4: "))
+if a==1:
+    print("Blue")
+elif a==2:
+    print("Green")
+elif a==3:
+    print("Red") 
+elif a==4:
+    print("Orange")  
+else:
+    print("Sorry no coulur for you!!!!")       
